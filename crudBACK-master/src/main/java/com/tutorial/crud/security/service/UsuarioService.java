04311a3af70a60bb0e3f6b808b47e46498a44cdf
etxeb <c.echeverria@ufromail.cl>
2021-02-02 17:59:54 -0300

    jwt ok

--- crudBACK-master/src/main/java/com/tutorial/crud/security/service/UsuarioService.java
+++ crudBACK-master/src/main/java/com/tutorial/crud/security/service/UsuarioService.java
@@ -1,8 +1,33 @@
 package com.tutorial.crud.security.service;
 
+import com.tutorial.crud.security.entity.Usuario;
+import com.tutorial.crud.security.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
+import javax.transaction.Transactional;
+import java.util.Optional;
 
+@Service
+@Transactional
 public class UsuarioService {
+
+    @Autowired
+    UsuarioRepository repo;
+
+    public Optional<Usuario> getByNombreUsuario(String nombreUsuario){
+        return repo.findByNombreUsuario(nombreUsuario);
+    }
+
+    public boolean existByNombreUsuario(String nombreUsuario){
+        return repo.existsByNombreUsuario(nombreUsuario);
+    }
+
+    public boolean existByEmail(String email){
+        return repo.existsByNombreUsuario(email);
+    }
+
+    public void save(Usuario usuario){
+        repo.save(usuario);
+    }
 }
