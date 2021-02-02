3687da6b11243a1cad9f000426ba7b6990f3677e
etxeb <c.echeverria@ufromail.cl>
2021-02-02 17:57:44 -0300

    jwt ok

---
+++ crudBACK-master/src/main/java/com/tutorial/crud/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,7 @@
+package com.tutorial.crud.security.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+public class UserDetailsServiceImpl implements UserDetails {
+}
---
+++ crudBACK-master/src/main/java/com/tutorial/crud/security/service/UsuarioService.java
@@ -0,0 +1,8 @@
+package com.tutorial.crud.security.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class UsuarioService {
+}
